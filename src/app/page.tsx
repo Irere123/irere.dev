41b8f331f71ae98a5c9de5cdf595d9c4bad706cf
Irere123 <irere2020@outlook.com>
2024-11-29 16:39:51 +0200

    use font mono

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <div className="flex justify-center items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="max-w-md space-y-3">
         <h1 className="text-2xl">Irere Emmanuel</h1>
-        <p className="italic">Software engineer</p>
-        <p>
+        <p className="font-mono">Software engineer</p>
+        <p className="font-mono">
           I&lsquo;m fluent in Go, Typescript, C, SQL, and English. I&lsquo;ve
           also written a good amount of JavaScript, HTML, C++, and Elixir. I
           learn quickly, care about detail, and love computer science and{" "}
